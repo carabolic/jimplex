307cc1a286fec60d16d3bcd0698a903f04e1a29e
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2012-01-09 16:16:29 +0100

    Added test file

---
+++ src/test/java/de/tu_berlin/coga/jimplex/SolverTest.java
@@ -0,0 +1,34 @@
+package de.tu_berlin.coga.jimplex;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for Solver.
+ */
+public class SolverTest extends TestCase {
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *          name of the test case
+	 */
+	public SolverTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(SolverTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testSolver() {
+		assertTrue(true);
+	}
+}
