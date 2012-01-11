4e70f93f27f87a3bbf22dc10643b0b8d8d2681c2
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2012-01-11 16:51:21 +0100

    Changed Solver to RevisedSimplex, add functionallity to convert any LP to standard form

--- src/main/java/de/tu_berlin/coga/jimplex/LinearProgram.java
+++ src/main/java/de/tu_berlin/coga/jimplex/LinearProgram.java
@@ -3,6 +3,11 @@ package de.tu_berlin.coga.jimplex;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class LinearProgram {
 	double[][] constraints;
@@ -17,6 +22,7 @@ public class LinearProgram {
 	String[] constraintNames;
 	String[] varName;
 	int numVariables;
+	int numSlackVariables;
 
 	public LinearProgram(String file) throws FileNotFoundException, ParseException, IOException {
 		LPReader reader = new LPReader(file);
@@ -60,6 +66,7 @@ public class LinearProgram {
 		constraintNames = reader.constrName;
 		varName = reader.varName;
 		numVariables = varName.length;
+		numSlackVariables = 0;
 	}
 
 	/**
@@ -78,9 +85,14 @@ public class LinearProgram {
 		// Count necessary slack variables (incomplete does not account for bounded
 		// variables e.g. 30 >= x <= 80)
 		int count = 0;
+		//Queue<Integer> slackIndices = new A
+		List<Integer> slackIndices = new ArrayList<Integer>(constraints.length);
+		Map<Integer, Integer> slackMap = new HashMap<Integer, Integer>(constraints.length);
+		
 		for (int i = 0; i < senses.length; i++) {
 			if (senses[i] != Sense.EQ) {
-				count++;
+				slackIndices.add(Integer.valueOf(i));
+				slackMap.put(count++, i);
 			}
 		}
 
@@ -89,14 +101,30 @@ public class LinearProgram {
 		double[] oldObjectives = obj;
 		int oldNumVariables = numVariables;
 
-		// constraints = new double[]
-
-		for (int i = 0; i < senses.length; i++) {
-			if (senses[i] == Sense.LEQ) {
-
-			}
+		numSlackVariables = count;
+		numVariables = numVariables + numSlackVariables;
+		obj = new double[numVariables];
+		constraints = new double[constraints.length][numVariables];
+		
+		System.arraycopy(oldObjectives, 0, obj, 0, oldNumVariables);
+		
+		for (int i = 0; i < constraints.length; i++) {
+			System.arraycopy(oldConstraints[i], 0, constraints[i], 0, oldNumVariables);
 		}
-		// <=
+		
+		for (Entry<Integer, Integer> slackMapping : slackMap.entrySet()) {
+			int constNum = slackMapping.getValue();
+			int slackNum = slackMapping.getKey();
+			
+			if (senses[constNum] == Sense.LEQ) {
+				constraints[constNum][numVariables - numSlackVariables + slackNum] = 1.0;
+			}
+			else {
+				constraints[constNum][numVariables - numSlackVariables + slackNum] = -1.0;
+			}
+			
+			senses[constNum] = Sense.EQ;
+		}
 	}
 
 	public static enum ObjectiveGoal {
